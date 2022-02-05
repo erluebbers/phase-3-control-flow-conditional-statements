8093d10720c083436f2992adc255e5d7a89794d7
Erik Luebbers <erluebbers@gmail.com>
2022-02-05 11:55:14 -0800

    Done.

--- control_flow.rb
+++ control_flow.rb
@@ -1,13 +1,33 @@
 def admin_login(username, password)
-  # your code here
+  if username == "admin" && password == "12345"
+    "Access granted"
+  elsif username == "ADMIN" && password =="12345"
+    "Access granted"
+  else 
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature >= 40 && temperature < 65
+    "It's a little chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else 
+    "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num/3 == "integer"
+    num/3
+  elsif num/5 == "integer"
+    "Buzz"
+  else  
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
