93219d2f80f2689cb7e7abdb802eb90ca735a91a
Erik Luebbers <erluebbers@gmail.com>
2022-02-05 12:36:15 -0800

    commit tree fc324dd46b43b2bb4b396ad2b48b560e6be298e2

--- control_flow.rb
+++ control_flow.rb
@@ -33,6 +33,11 @@ def fizzbuzz(num)
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+" then num1 + num2
+  when "-" then num1 - num2
+  when "*" then num1 * num2
+  when "/" then num1 / num2
+  else "Invalid operation!"
 end
 
